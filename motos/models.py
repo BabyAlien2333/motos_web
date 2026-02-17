7339e7eef4b5379a8f54a76380f8b6cbae993e55
camilo <jisuspogba10@gmail.com>
2026-02-17 13:58:10 -0500

    use CloudinaryField for image uploads

--- motos/models.py
+++ motos/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models import Avg
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 class Moto(models.Model):
     marca = models.CharField(max_length=100)
@@ -11,7 +13,7 @@ class Moto(models.Model):
     precio = models.DecimalField(max_digits=12, decimal_places=0)
 
     descripcion = models.TextField(blank=True)
-    imagen = models.ImageField(upload_to='motos/', null=True, blank=True)
+    imagen = CloudinaryField('imagen', blank=True, null=True)
 
     def promedio_estrellas(self):
         promedio = self.resenas.aggregate(Avg('estrellas'))['estrellas__avg']
@@ -22,17 +24,18 @@ class Moto(models.Model):
 
 
 class Resena(models.Model):
-    moto = models.ForeignKey(Moto, on_delete=models.CASCADE, related_name='resenas')
-    
-    
-
+    moto = models.ForeignKey(
+        Moto,
+        on_delete=models.CASCADE,
+        related_name='resenas'
+    )
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-
     comentario = models.TextField()
     estrellas = models.IntegerField()
-    imagen = models.ImageField(upload_to='resenas/', null=True, blank=True)
+    imagen = CloudinaryField('imagen', blank=True, null=True)
     fecha = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.usuario} - {self.moto}"
+
 
